81bbcc6ef02bcd48f61b8e9af154923881b694c2
rashmiwani0102 <rashmiwani2007@gmail.com>
2026-09-19 19:58:15 +0530

    Create substractionbyenteringdigit.c

---
+++ substractionbyenteringdigit.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter 1st number: ");
+    scanf("%d",&a);
+    printf("Enter 2nd number: ");
+    scanf("%d",&b);
+    int c=a-b;
+    printf("differnce of the numbers is %d",c);
+    return 0;
+}
